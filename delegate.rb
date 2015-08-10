35388cffaabc78455f9afe81e8d0d0d3dbeb3a9b
Zachary Perlmutter <zrp200@gmail.com>
2015-08-10 13:02:51 -0700

    Update delegate.rb

--- delegate.rb
+++ delegate.rb
@@ -1,8 +1,15 @@
 module Delegate
 	def method_missing(method, *args, &block)
-		if @delegete.respond_to? method then @delegete.public_send method, *args, &block
-		else super end
+		if @delegete.respond_to? method
+			@delegete.public_send(method, *args, &block)
+		else 
+			super
+		end
 	end
+	def respond_to?(method)
+		@delegete.respond_to?(method) || super
+	end
+	private
 	def delegate(object)
 		@delegete = object
 	end
